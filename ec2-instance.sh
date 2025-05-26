621f8433f0d9f2295ea9884dc499a1fa76ab8173
Priyanka <priyanka.rel6@gmail.com>
2025-05-26 12:23:49 +0530

    created script file for user cart and redis

--- ec2-instance.sh
+++ ec2-instance.sh
@@ -1,21 +1,40 @@
 #!/bin/bash
 
 AMI_ID="ami-09c813fb71547fc4f"
-INSTANCE_TYPE="t2.micro"
-SG_ID="sg-02d7436ae856ae341"
+SG_ID="sg-02d7436ae856ae341" # replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
+ZONE_ID="Z06734122W0TQFHN7RZBR" # replace with your ZONE ID
+DOMAIN_NAME="ravada.site" # replace with your domain
 
-for INSTANCE in "${INSTNACES[@]}"; 
+#for instance in ${INSTANCES[@]}
+for instance in $@
 do
-  echo "Creating $INSTANCE instance"
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-02d7436ae856ae341 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    if [ $instance != "frontend" ]
+    then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME"
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+        RECORD_NAME="$DOMAIN_NAME"
+    fi
+    echo "$instance IP address: $IP"
 
-  INSTANCE_ID=$(aws ec2 run-instances \
-    --image-id "$AMI_ID" \
-    --instance-type "$INSTANCE_TYPE" \
-    --security-group-ids "$SECURITY_GROUP_ID" \
-    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance-latest},{Key=service,Value=$instance}]" \
-    --query "Instances[0].InstanceId" \
-    --output text)
-
-  echo "Created instance with ID: $INSTANCE_ID"
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Creating or Updating a record set for cognito endpoint"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$RECORD_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP'"
+            }]
+        }
+        }]
+    }'
 done
